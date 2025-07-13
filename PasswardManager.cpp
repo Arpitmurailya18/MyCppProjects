eb8eae70eeb2cd0e1e8688ab8a7cd8349c21bc10
Arpitmurailya18 <152486610+Arpitmurailya18@users.noreply.github.com>
2025-07-14 01:22:17 +0530

    Add files via upload

---
+++ PasswardManager.cpp
@@ -0,0 +1,41 @@
+#include<iostream> 
+#include<fstream>
+#include<string> //for string type data
+#include<vector> //container
+
+using namespace std;
+
+//creating costom data type
+struct User{
+    string username;
+    string password;
+    string Accountname;
+};
+
+
+class PasswordManager{
+    private:
+        string LoginUserName;
+        string LoginPassword;
+        vector<User> ListOfUsers; // container
+    
+    public:
+        void saveToFile(); //To save the data
+        void LoadToFile();  //To load the data
+        void MainMenu();
+        void addUser();  // TO add the user
+        void deleteUser();  // to delete the User
+        void updateUser();  // To display the User data
+        void displayUser();  //To display the user data
+        void searchUser(); //To search the user 
+        void xorEncryptDecrypt(string &data, const string &key);
+        void registerUser();  // To register the user 
+        void LoginMainMenu();
+        void login();  //For user login
+        void logout();  // for user logout
+};
+
+
+void PasswordManager::saveToFile(){
+    
+}
